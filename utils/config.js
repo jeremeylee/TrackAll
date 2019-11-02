1b18e918cf134701124c949611773e7c756b4c19
Jeremey Lee <leejeremey@gmail.com>
2019-11-02 16:43:10 -0700

    Changed if statement to check for development instead of test

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const { PORT } = process.env;
 let { MONGODB_URI } = process.env;
-if (process.env.NODE_ENV === "test") {
+if (process.env.NODE_ENV === "development") {
 	MONGODB_URI = process.env.TEST_MONGODB_URI;
 }
 
