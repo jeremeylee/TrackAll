ce31d3050d1810b497457addd7f79957b366f8d8
Jeremey Lee <leejeremey@gmail.com>
2019-11-02 16:19:10 -0700

    Added GET and POST requests

--- controllers/lists.js
+++ controllers/lists.js
@@ -1,7 +1,24 @@
 const listsRouter = require("express").Router();
+const User = require("../models/list");
 
 listsRouter.get("/", async (req, res) => {
-	res.send("hello world");
+	const allLists = await User.find({});
+	res.json(allLists.map(list => list.toJSON()));
+});
+
+listsRotuer.post("/", async (req, res, next) => {
+	const { body } = request;
+
+	try {
+		const newList = new Blog({
+			content: body.content,
+		});
+
+		const savedList = await newList.save();
+		res.status(201).json(savedList.toJSON());
+	} catch (exception) {
+		next(exception);
+	}
 });
 
 module.exports = listsRouter;
