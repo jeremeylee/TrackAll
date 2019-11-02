49b791115e87a319c5abcf65603309e0711180ac
Jeremey Lee <leejeremey@gmail.com>
2019-11-02 15:06:10 -0700

    Messing with eslint and prettier

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const listsRouter = require('./controllers/lists');
+const express = require("express");
+const bodyParser = require("body-parser");
+const listsRouter = require("./controllers/lists");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/api/lists', listsRouter);
+app.use("/api/lists", listsRouter);
 module.exports = app;
