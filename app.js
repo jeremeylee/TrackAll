4b17719a0d9931a921a40c5fad5212052435fd9f
Jeremey Lee <leejeremey@gmail.com>
2019-11-02 15:53:46 -0700

    Setup mongoose connection to MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const listsRouter = require("./controllers/lists");
+const config = require("./utils/config");
 
 const app = express();
+
+console.log("Connecting to ", config.MONGODB_URI);
+
+mongoose
+	.connect(config.MONGODB_URI, { useNewUrlParser: true })
+	.then(() => {
+		console.log("connected to MongoDB");
+	})
+	.catch(error => {
+		console.log("error while connecting to MongoDB: ", error.message);
+	});
 
 app.use(bodyParser.json());
 app.use("/api/lists", listsRouter);
