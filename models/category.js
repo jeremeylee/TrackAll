63e678d58b7aa58f5fa181801bcc3262ebd985ac
Jeremey Lee <leejeremey@gmail.com>
2019-11-02 16:48:26 -0700

    Temporarily removed ref to lists

--- models/category.js
+++ models/category.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
 	title: String,
-	lists: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "List",
-		},
-	],
+	// lists: [
+	// 	{
+	// 		type: mongoose.Schema.Types.ObjectId,
+	// 		ref: "List",
+	// 	},
+	// ],
 });
 
 const Category = mongoose.model("Category", categorySchema);
