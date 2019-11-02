7edb68953d53b0d1ae4fdece56d7a77f5c61dd80
Jeremey Lee <leejeremey@gmail.com>
2019-11-02 16:30:40 -0700

    Created PUT and DELETE requests

--- controllers/lists.js
+++ controllers/lists.js
@@ -21,4 +21,27 @@ listsRouter.post("/", async (req, res, next) => {
 	}
 });
 
+listsRouter.delete("/:id", async (req, res, next) => {
+	const { body } = req;
+	await List.findByIdAndDelete(body.id);
+	res.status(204).end();
+});
+
+listsRouter.put("/:id", async (req, res, next) => {
+	const { body } = req;
+	try {
+		const updatedList = await List.findByIdAndUpdate(
+			body.id,
+			{
+				content: body.content,
+			},
+			{ new: true }
+		);
+
+		res.status(201).json(updatedList.toJSON());
+	} catch (exception) {
+		next(exception);
+	}
+});
+
 module.exports = listsRouter;
