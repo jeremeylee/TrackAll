d3ce410ac7bb0628a1f719dafe079c23a6185b07
Jeremey Lee <leejeremey@gmail.com>
2019-11-02 15:32:37 -0700

    Created list schema

---
+++ models/list.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const listSchema = new mongoose.Schema({
+	content: String,
+	category: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Category",
+		},
+	],
+});
+
+const List = mongoose.model("List", listSchema);
+
+module.exports = List;
