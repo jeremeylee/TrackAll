a96b6e4377dfd5617f6a3872b4b8fa2b72245761
Jeremey Lee <leejeremey@gmail.com>
2019-11-02 23:09:29 -0700

    Implemented bottom nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Navigation from "./components/Navigation";
-
+import BottomNav from "./components/BottomNav";
 const App = () => {
 	return (
 		<div>
 			<Navigation />
+			<BottomNav />
 		</div>
 	);
 };
