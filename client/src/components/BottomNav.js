d4733ac4830432eafff46c7d5f32bd5ae3d436ec
Jeremey Lee <leejeremey@gmail.com>
2019-11-02 23:16:17 -0700

    Made bottomnav stick to bottom

--- client/src/components/BottomNav.js
+++ client/src/components/BottomNav.js
@@ -3,13 +3,24 @@ import {
 	BottomNavigation,
 	BottomNavigationAction,
 	useMediaQuery,
+	makeStyles,
 } from "@material-ui/core";
 import HomeIcon from "@material-ui/icons/Home";
 import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
 
+const useStyles = makeStyles({
+	sticky: {
+		width: "100%",
+		position: "fixed",
+		bottom: 0,
+	},
+});
+
 const BottomNav = props => {
 	const [value, setValue] = useState(0);
 	const matches = useMediaQuery("(min-width:1024px)");
+	const classes = useStyles();
+
 	return (
 		<BottomNavigation
 			showLabels
@@ -18,6 +29,7 @@ const BottomNav = props => {
 			onChange={(event, newValue) => {
 				setValue(newValue);
 			}}
+			className={classes.sticky}
 		>
 			<BottomNavigationAction label="Home" icon={<HomeIcon />} />
 			<BottomNavigationAction label="All" icon={<LibraryBooksIcon />} />
