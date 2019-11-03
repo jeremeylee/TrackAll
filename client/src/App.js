88d6ec588059e26d85097bfca7de3f8d1417affe
Jeremey Lee <leejeremey@gmail.com>
2019-11-03 00:25:29 -0700

    Implemented Home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Navigation from "./components/Navigation";
 import BottomNav from "./components/BottomNav";
+import Home from "./components/Home";
+
 const App = () => {
-	return <div></div>;
+	return (
+		<div>
+			<Home />
+		</div>
+	);
 };
 
 export default App;
