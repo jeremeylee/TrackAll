3b8e212ee3dbbeb68ebb405bcc2bfaf67840903e
Jeremey Lee <leejeremey@gmail.com>
2019-11-03 00:55:44 -0700

    Implemented home card

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import categoryService from "../services/categories";
-
+import HomeCard from "./HomeCard";
 const Home = props => {
 	const [categories, setCategories] = useState([]);
 	useEffect(() => {
@@ -9,7 +9,11 @@ const Home = props => {
 		});
 	}, []);
 	console.log(categories);
-	return <div>Home</div>;
+	return (
+		<div>
+			<HomeCard />
+		</div>
+	);
 };
 
 export default Home;
