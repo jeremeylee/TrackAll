d82b81f1b292e4d0c5d751f8ee62d62288424e9b
Jeremey Lee <leejeremey@gmail.com>
2019-11-02 17:53:24 -0700

    Reintroduced ref features for category and list

--- controllers/lists.js
+++ controllers/lists.js
@@ -1,5 +1,6 @@
 const listsRouter = require("express").Router();
 const List = require("../models/list");
+const Category = require("../models/category");
 
 listsRouter.get("/", async (req, res) => {
 	const allLists = await List.find({});
@@ -10,11 +11,16 @@ listsRouter.post("/", async (req, res, next) => {
 	const { body } = req;
 
 	try {
+		const category = await Category.findById(body.categoryId);
+
 		const newList = new List({
 			content: body.content,
+			category: category._id,
 		});
 
 		const savedList = await newList.save();
+		category.lists = category.lists.concat(savedList._id);
+		await category.save();
 		res.status(201).json(savedList.toJSON());
 	} catch (exception) {
 		next(exception);
@@ -32,16 +38,28 @@ listsRouter.delete("/:id", async (req, res, next) => {
 
 listsRouter.put("/:id", async (req, res, next) => {
 	const { body } = req;
-
 	try {
+		const newCategory = await Category.findById(body.newCategoryId);
+
 		const updatedList = await List.findByIdAndUpdate(
 			req.params.id,
 			{
 				content: body.content,
+				category: newCategory._id,
 			},
 			{ new: true }
 		);
 
+		if (body.newCategoryId !== body.oldCategoryId) {
+			const oldCategory = await Category.findById(body.oldCategoryId);
+			oldCategory.lists = oldCategory.lists.filter(
+				list => list.toHexString() !== updatedList._id.toHexString()
+			);
+
+			await oldCategory.save();
+			newCategory.lists = newCategory.lists.concat(updatedList._id);
+			await newCategory.save();
+		}
 		res.status(201).json(updatedList.toJSON());
 	} catch (exception) {
 		next(exception);
