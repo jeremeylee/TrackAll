80b4d6779e4900c19bccafaa0fdb29eebb0a1c54
Jeremey Lee <leejeremey@gmail.com>
2019-11-03 00:09:44 -0700

    Removed both navs for now

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,7 @@ import React from "react";
 import Navigation from "./components/Navigation";
 import BottomNav from "./components/BottomNav";
 const App = () => {
-	return (
-		<div>
-			<Navigation />
-			<BottomNav />
-		</div>
-	);
+	return <div></div>;
 };
 
 export default App;
