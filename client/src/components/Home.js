7204f56ad14270c1252c53e748afba3e324fb996
Jeremey Lee <leejeremey@gmail.com>
2019-11-03 00:34:33 -0700

    Get all current categories from DB

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import categoryService from "../services/categories";
 
 const Home = props => {
+	const [categories, setCategories] = useState([]);
+	useEffect(() => {
+		categoryService.getCategories().then(initialCategories => {
+			setCategories(initialCategories);
+		});
+	}, []);
+	console.log(categories);
 	return <div>Home</div>;
 };
 
