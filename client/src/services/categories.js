8742b6ec7b314897d2a071d6974360ff4ff687d4
Jeremey Lee <leejeremey@gmail.com>
2019-11-03 00:32:47 -0700

    Fixed typo

--- client/src/services/categories.js
+++ client/src/services/categories.js
@@ -1,11 +1,11 @@
-import axios from ('axios');
-const baseUrl = 'http://localhost:3003';
+import axios from "axios";
+const baseUrl = "http://localhost:3003";
 
 const getCategories = () => {
-  const request = axios.get(`${baseUrl}/categories`);
-  return request.then(response => response.data);
+	const request = axios.get(`${baseUrl}/categories`);
+	return request.then(response => response.data);
 };
 
 export default {
-  getCategories,
+	getCategories,
 };
