e1bd963f8174a827da7e43868f0847f9074cb88e
Jeremey Lee <leejeremey@gmail.com>
2019-11-03 01:06:28 -0700

    Get all lists

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from "react";
 import categoryService from "../services/categories";
+import listService from "../services/lists";
 import HomeCard from "./HomeCard";
 const Home = props => {
 	const [categories, setCategories] = useState([]);
+	const [lists, setLists] = useState([]);
 	useEffect(() => {
 		categoryService.getCategories().then(initialCategories => {
 			setCategories(initialCategories);
 		});
+		listService.getLists().then(initialLists => {
+			setLists(initialLists);
+		});
 	}, []);
 	console.log(categories);
+	console.log(lists);
 	return (
 		<div>
 			<HomeCard />
