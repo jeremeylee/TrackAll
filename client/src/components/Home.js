d1bfedacecd57ebe56beb98b15d47f5d352a545b
Jeremey Lee <leejeremey@gmail.com>
2019-11-07 12:38:10 -0800

    Removed list service from Home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,19 +1,13 @@
 import React, { useState, useEffect } from "react";
 import categoryService from "../services/categories";
-import listService from "../services/lists";
 import HomeCard from "./HomeCard";
 const Home = props => {
 	const [categories, setCategories] = useState([]);
-	const [lists, setLists] = useState([]);
+
 	useEffect(() => {
 		//Get all the categories in the db
 		categoryService.getCategories().then(initialCategories => {
 			setCategories(initialCategories);
-		});
-
-		//Get all the lists in the db
-		listService.getLists().then(initialLists => {
-			setLists(initialLists);
 		});
 	}, []);
 
