74ca990936ee248ec60f39deaa4b2a89c600a8d0
Jeremey Lee <leejeremey@gmail.com>
2019-11-07 14:46:40 -0800

    Lifted categories service into App.js and passed as props into Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
+import categoryService from "./services/categories";
 import Home from "./components/Home";
+import Category from "./components/Category";
 
 const App = () => {
+	const [categories, setCategories] = useState([]);
+
+	useEffect(() => {
+		//Get all the categories in the db
+		categoryService.getCategories().then(initialCategories => {
+			setCategories(initialCategories);
+		});
+	}, []);
 	return (
 		<div>
 			<Switch>
 				<Route path="/">
-					<Home />
+					{/* <Category /> */}
+					<Home categories={categories} />
 				</Route>
 			</Switch>
 		</div>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,28 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Grid, Typography } from "@material-ui/core";
-import categoryService from "../services/categories";
 import HomeCard from "./HomeCard";
 import Navigation from "./Navigation";
 import BottomNav from "./BottomNav";
 import AddIcon from "@material-ui/icons/Add";
 
 const Home = props => {
-	const [categories, setCategories] = useState([]);
-
-	useEffect(() => {
-		//Get all the categories in the db
-		categoryService.getCategories().then(initialCategories => {
-			setCategories(initialCategories);
-		});
-	}, []);
-
 	return (
 		<div>
 			<Navigation
 				toolBarContent={<Typography variant="h6">Track All</Typography>}
 			/>
 			<Grid container spacing={0}>
-				{categories.map(category => (
+				{props.categories.map(category => (
 					<Grid item xs={12} sm={6} lg={3} key={category.id}>
 						<HomeCard title={category.title} lists={category.lists} />
 					</Grid>
