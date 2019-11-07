3cc1db05425deb4dd05297e146dcc098cb611afe
Jeremey Lee <leejeremey@gmail.com>
2019-11-07 15:01:11 -0800

    Changed List component to ListContent

---
+++ client/src/components/ListContent.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { List, ListItem, ListItemText } from "@material-ui/core";
+const ListContent = props => {
+	return (
+		<List>
+			{props.lists.map(list => (
+				<ListItem key={list.id} divider={true}>
+					<ListItemText primary={list.content} />
+				</ListItem>
+			))}
+		</List>
+	);
+};
+
+export default ListContent;
