f569c8f2206410b1e735474de8df6ad74433afc5
Jeremey Lee <leejeremey@gmail.com>
2019-11-07 12:53:04 -0800

    Changed toolbar content to be a prop

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, useMediaQuery } from "@material-ui/core";
+import { AppBar, Toolbar } from "@material-ui/core";
 
 const Navigation = props => {
-	const matches = useMediaQuery("(min-width:1024px)");
+	// const matches = useMediaQuery("(min-width:1024px)");
 
 	return (
-		<AppBar position="sticky" style={{ display: matches ? "" : "none" }}>
-			<Toolbar>
-				<Typography variant="h6">Track All</Typography>
-			</Toolbar>
+		<AppBar position="sticky">
+			<Toolbar>{props.toolBarContent}</Toolbar>
 		</AppBar>
 	);
 };
