57aa21fee01fe4cc792ec561cc57f58a61603802
Jeremey Lee <leejeremey@gmail.com>
2019-11-07 16:39:40 -0800

    Added category POST request service

--- client/src/services/categories.js
+++ client/src/services/categories.js
@@ -6,6 +6,12 @@ const getCategories = () => {
 	return request.then(response => response.data);
 };
 
+const addCategory = async newCategory => {
+	const request = await axios.post("/categories", newCategory);
+	return request;
+};
+
 export default {
 	getCategories,
+	addCategory,
 };
