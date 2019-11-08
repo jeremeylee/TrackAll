4b8b80b8a9afbc59c419622e2b3d43e2c1f70a67
Jeremey Lee <leejeremey@gmail.com>
2019-11-07 16:24:15 -0800

    Added credit for code snippet

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,6 +8,7 @@ import AddIcon from "@material-ui/icons/Add";
 
 const randomID = () => {
 	return (
+		// https://gist.github.com/6174/6062387
 		Math.random()
 			.toString(36)
 			.substring(2, 15) +
