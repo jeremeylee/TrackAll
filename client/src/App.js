c234e04de0469cb698d7253a30be99890395b0a4
Jeremey Lee <leejeremey@gmail.com>
2019-11-07 17:51:44 -0800

    Implemented feature to retrieve all lists from db

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import categoryService from "./services/categories";
+import listService from "./services/lists";
 import Home from "./components/Home";
 import Category from "./components/Category";
 
 const App = () => {
 	const [categories, setCategories] = useState([]);
+	const [lists, setLists] = useState([]);
 
 	useEffect(() => {
 		//Get all the categories in the db
 		categoryService.getCategories().then(initialCategories => {
 			setCategories(initialCategories);
+		});
+
+		listService.getLists().then(initialLists => {
+			setLists(initialLists);
 		});
 	}, []);
 	return (
