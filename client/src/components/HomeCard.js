4067020fffcd47a73220c81af47a157198f94543
Jeremey Lee <leejeremey@gmail.com>
2019-11-16 14:30:08 -0800

    Created dialog menu for edit and deletes

--- client/src/components/HomeCard.js
+++ client/src/components/HomeCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import ListContent from "./ListContent";
 import {
@@ -7,10 +7,20 @@ import {
 	CardContent,
 	Typography,
 	Button,
+	Dialog,
+	List,
+	ListItem,
+	ListItemText,
 	makeStyles,
 } from "@material-ui/core";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 const useStyles = makeStyles({
+	title: {
+		display: "flex",
+		justifyContent: "space-between",
+	},
+
 	card: {
 		display: "flex",
 		flexDirection: "column",
@@ -29,12 +39,64 @@ const useStyles = makeStyles({
 });
 
 const HomeCard = props => {
+	const [open, setOpen] = useState(false);
 	const classes = useStyles();
+	const settings = ["Edit", "Delete"];
+
+	const handleClick = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const handleListItemClick = item => {
+		switch (item) {
+			case "Edit":
+				handleEdit();
+				break;
+			case "Delete":
+				handleDelete();
+				break;
+			default:
+				setOpen(false);
+				break;
+		}
+	};
+
+	const handleEdit = () => {
+		console.log("edit");
+		setOpen(false);
+	};
+
+	const handleDelete = () => {
+		console.log("delete");
+		setOpen(false);
+	};
 
 	return (
 		<Card className={classes.card}>
 			<CardContent className={classes.cardContent}>
-				<Typography variant="h5">{props.title}</Typography>
+				<div className={classes.title}>
+					<Typography variant="h5">{props.title}</Typography>
+					<Button onClick={handleClick}>
+						<MoreHorizIcon />
+					</Button>
+				</div>
+				<Dialog onClose={handleClose} open={open}>
+					<List>
+						{settings.map(setting => (
+							<ListItem
+								button
+								onClick={() => handleListItemClick(setting)}
+								key={setting}
+							>
+								<ListItemText>{setting}</ListItemText>
+							</ListItem>
+						))}
+					</List>
+				</Dialog>
 				<ListContent lists={props.lists} />
 			</CardContent>
 			<CardActions className={classes.cardActions}>
