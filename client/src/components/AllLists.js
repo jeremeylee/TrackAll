42bc2c4253884af2357f7ad7b44d9bd80032c818
Jeremey Lee <leejeremey@gmail.com>
2019-11-19 16:44:31 -0800

    On item select, show delete and edit icons

--- client/src/components/AllLists.js
+++ client/src/components/AllLists.js
@@ -1,15 +1,48 @@
-import React from "react";
-import { List, ListItem, ListItemText } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+	List,
+	ListItem,
+	ListItemText,
+	Button,
+	makeStyles,
+} from "@material-ui/core";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+const useStyles = makeStyles({
+	flex: {
+		display: "flex",
+		justifyContent: "space-between",
+	},
+});
 
 const ListContent = props => {
+	const [selectMode, setSelectMode] = useState("");
+	const classes = useStyles();
+
+	const handleSelectMode = id => {
+		setSelectMode(id);
+	};
+
 	return (
 		<List>
 			{props.lists === undefined
 				? ""
 				: props.lists.map(list => (
-						<ListItem key={list.id} divider={true}>
-							<ListItemText primary={list.content} />
-						</ListItem>
+						<div key={list.id} className={classes.flex}>
+							<ListItem divider={true}>
+								<ListItemText
+									onClick={() => handleSelectMode(list.id)}
+									primary={list.content}
+								/>
+								<Button
+									style={{ display: list.id !== selectMode ? "none" : "" }}
+								>
+									<EditIcon />
+									<DeleteIcon />
+								</Button>
+							</ListItem>
+						</div>
 				  ))}
 		</List>
 	);
