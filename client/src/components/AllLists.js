61bab6425b57ab873c5483e59edc93719708d5f9
Jeremey Lee <leejeremey@gmail.com>
2019-11-19 17:27:12 -0800

    Show item text when editing

--- client/src/components/AllLists.js
+++ client/src/components/AllLists.js
@@ -4,6 +4,10 @@ import {
 	ListItem,
 	ListItemText,
 	Button,
+	Dialog,
+	DialogContent,
+	DialogActions,
+	TextField,
 	makeStyles,
 } from "@material-ui/core";
 import EditIcon from "@material-ui/icons/Edit";
@@ -17,25 +21,45 @@ const useStyles = makeStyles({
 });
 
 const ListContent = props => {
+	const listContent = props.lists.map(list => list.content);
 	const [selectMode, setSelectMode] = useState("");
+	const [index, setIndex] = useState(null);
+	const [openEdit, setOpenEdit] = useState(false);
+	const [editValue, setEditValue] = useState(listContent);
 	const classes = useStyles();
-
-	const handleSelectMode = id => {
+	console.log(listContent);
+	const handleSelect = (id, idx) => {
 		setSelectMode(id);
+		setIndex(idx);
 	};
 
+	const handleEditMenuOpen = () => {
+		setOpenEdit(true);
+	};
+	const handleEditMenuClose = () => {
+		setOpenEdit(false);
+	};
+
+	const handleEditValueChange = event => {
+		setEditValue(event.target.value);
+	};
+
+	const handleEdit = () => {
+		console.log("edit");
+	};
 	return (
 		<List>
 			{props.lists === undefined
 				? ""
-				: props.lists.map(list => (
+				: props.lists.map((list, index) => (
 						<div key={list.id} className={classes.flex}>
 							<ListItem divider={true}>
 								<ListItemText
-									onClick={() => handleSelectMode(list.id)}
+									onClick={() => handleSelect(list.id, index)}
 									primary={list.content}
 								/>
 								<Button
+									onClick={handleEditMenuOpen}
 									style={{ display: list.id !== selectMode ? "none" : "" }}
 								>
 									<EditIcon />
@@ -48,6 +72,31 @@ const ListContent = props => {
 							</ListItem>
 						</div>
 				  ))}
+
+			<Dialog onClose={handleEditMenuClose} open={openEdit} fullWidth>
+				<form autoComplete="off" onSubmit={handleEdit}>
+					<DialogContent>
+						<TextField
+							autoFocus
+							margin="dense"
+							id="edit"
+							value={editValue[index]}
+							onChange={handleEditValueChange}
+							label="Edit your item"
+							type="edit"
+							fullWidth
+						/>
+						<DialogActions>
+							<Button onClick={handleEditMenuClose} color="primary">
+								Cancel
+							</Button>
+							<Button color="primary" type="submit">
+								Save
+							</Button>
+						</DialogActions>
+					</DialogContent>
+				</form>
+			</Dialog>
 		</List>
 	);
 };
