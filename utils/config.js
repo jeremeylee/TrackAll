8f569da3ca48ac9cc24a928aa5ec261f58f49ff5
Jeremey Lee <leejeremey@gmail.com>
2019-12-03 17:14:43 -0800

    Made adjustments to the PORT being connected to

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const { PORT } = process.env;
+const PORT = process.env.PORT || 8081;
 let { MONGODB_URI } = process.env;
 if (process.env.NODE_ENV === "development") {
 	MONGODB_URI = process.env.TEST_MONGODB_URI;
