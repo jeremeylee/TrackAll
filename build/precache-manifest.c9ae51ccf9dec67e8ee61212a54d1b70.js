a92e7b13e222aa0f8c2cac8189b318f471a5f13f
Jeremey Lee <leejeremey@gmail.com>
2019-12-03 18:29:25 -0800

    Build folder

---
+++ build/precache-manifest.c9ae51ccf9dec67e8ee61212a54d1b70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84cb1c6e242167a1307cfe35754a53bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0b434270151a1ab0640",
+    "url": "/static/css/main.cff04494.chunk.css"
+  },
+  {
+    "revision": "22e2ccc7b2073115a0c5",
+    "url": "/static/js/2.b902c6c8.chunk.js"
+  },
+  {
+    "revision": "e0b434270151a1ab0640",
+    "url": "/static/js/main.d216248b.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
